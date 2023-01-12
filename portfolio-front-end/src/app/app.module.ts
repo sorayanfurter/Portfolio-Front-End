5fb6a1ba945d064a7fae8e91c1837178e02a3a85
Soraya <sorinfurter@gmail.com>
2023-01-12 00:58:50 -0300

    Projects section+cards

--- portfolio-front-end/src/app/app.module.ts
+++ portfolio-front-end/src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { AboutComponent } from './about/about.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { AboutComponent } from './Components/about/about.component';
+import { LandingPageComponent } from './Components/landing-page/landing-page.component';
+import { ProjectsComponent } from './Components/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AboutComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
